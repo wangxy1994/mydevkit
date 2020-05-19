79192b108c1e4ee8e3ea12ea719f22a0a8d079a9
wangxy1994 <1076507534@qq.com>
2020-05-19 18:58:11 +0800

    改造工具-临时提交

--- exoskeleton/src/main/java/com/wangxy/exoskeleton/controller/CaveController.java
+++ exoskeleton/src/main/java/com/wangxy/exoskeleton/controller/CaveController.java
@@ -2,24 +2,31 @@ package com.wangxy.exoskeleton.controller;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import com.alibaba.fastjson.JSONArray;
-import com.alibaba.fastjson.JSONObject;
-import com.wangxy.exoskeleton.api.BaiduTranslateUtil;
-import com.wangxy.exoskeleton.entity.TranslateResult;
 import org.apache.commons.io.FileUtils;
 import org.apache.ibatis.annotations.Param;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.DefaultResourceLoader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import com.wangxy.exoskeleton.api.BaiduTranslateUtil;
 import com.wangxy.exoskeleton.entity.Pagelable;
+import com.wangxy.exoskeleton.entity.TranslateResult;
 import com.wangxy.exoskeleton.service.IPageLableService;
 
 @RestController
@@ -32,26 +39,30 @@ public class CaveController {
 
 	@RequestMapping("/entrance")
 	public String entrance(@Param("path") String path) throws IOException {
-		System.out.println(99999999);
-//		path = "G:/同步dev/国际化/virtualcombiBefore/virtualCombiList.jsp";
-		path = "I:/同步dev/国际化/virtualcombiBefore/virtualCombiList.jsp";
+		System.out.println("path==="+path);
+		if (path.trim().length()==0) {
+			path = "G:/同步dev/国际化/virtualcombiBefore/virtualCombiList.jsp";
+		}
+		// path = "I:/同步dev/国际化/virtualcombiBefore/virtualCombiList.jsp";
 		// path = "D:/work/IRM/avengers-systemweb/src/main/webapp/ifm/pub/js/transdate/TransDateSet.js";
 		// String content = IOUtils.toString(resourceLoader .getResource(path).getInputStream());
-		List<String> list = FileUtils.readLines(new File(path));
 		//
 		// (path);
-		// jsoupDeal(path);
+		String[] pathPart = path.split("\\.");
+		String[] folderAndFile = pathPart[0].split("/");
+		String pageId = folderAndFile[folderAndFile.length - 1];
 		// html处理
+		//htmlCodeDeal(path, pageId);
 
+		List<String> list = FileUtils.readLines(new File(path));
 		// js代码处理。
 		// 固定替换
-		List<String> resultList = apachCommonDeal(list);
-		//固定替换后进行翻译替换
-		resultList = translateDeal(resultList);
+		List<String> resultList = fixReplaceDeal(list);
+		// 固定替换后进行翻译替换
+		// resultList = translateDeal(resultList);
 
+		// 生成文件
 
-		//生成文件
-		String[] pathPart = path.split("\\.");
 		String resultPath = pathPart[0] + "_res_." + pathPart[1];
 		File resultFile = new File(resultPath);
 		FileUtils.writeLines(resultFile, resultList);
@@ -62,7 +73,36 @@ public class CaveController {
 		return pagelable.getLableInfo();
 	}
 
-	private List<String> apachCommonDeal(List<String> oriList) throws IOException {
+	private void htmlCodeDeal(String path, String pageId) throws IOException {
+		Map<String, String> chineseWords = jsoupDeal(path);
+
+		// 翻译后，产生tsys_pagelable对象，用这个对象来产生i18N脚本，把对象插入数据库
+		// TODO 获取最大id
+		int id = 901000;
+		id = id/100*100;
+		id += 100;
+		List<TranslateResult> tr = new ArrayList<TranslateResult>();
+		for (Map.Entry<String, String> entry : chineseWords.entrySet()) {
+			// System.out.println("Key = " + entry.getKey() + ", Value = " + entry.getValue());
+			TranslateResult res = new TranslateResult();
+			res.setPageId(pageId);
+			res.setLabelId(String.valueOf(id));
+			res.setSource(entry.getKey());
+			String transResult = BaiduTranslateUtil.getApi().getTransResult(res.getSource(), "auto", "en");
+			res.setTranslateResult(transResult);
+			tr.add(res);
+			id++;
+		}
+		// 产生翻译表，产生脚本，插入数据库
+
+		// pageLableService.addPagelable(pagelable);
+
+		// 根据数据库,用中文查询对应的标签语法，并替换到文件中
+
+		// 数据字典
+	}
+
+	private List<String> fixReplaceDeal(List<String> oriList) throws IOException {
 		List<String> reultLines = new ArrayList<>();
 		StringBuilder sb = new StringBuilder();
 		for (String ss : oriList) {
@@ -78,11 +118,23 @@ public class CaveController {
 		return reultLines;
 	}
 
+	/**
+	 * 剩余部分翻译
+	 * @param oriList
+	 * @return
+	 * @throws IOException
+	 */
 	private List<String> translateDeal(List<String> oriList) throws IOException {
 		Set<String> lineToTranslate = new HashSet<String>();
-
+		//这里只翻译js脚本中
+		boolean isJsCode = false;
 		for (String ss : oriList) {
-			lineToTranslate.addAll(getTranslateLine(ss));
+			if (ss.contains("<script>")) {
+				isJsCode = true;
+			}
+			if (isJsCode) {
+				lineToTranslate.addAll(getTranslateLine(ss));
+			}
 		}
 		// System.out.println(sb.toString());
 		//翻译产生map集合
@@ -102,6 +154,11 @@ public class CaveController {
 		return reultLines;
 	}
 
+	/**
+	 * 产生翻译结果对象集合
+	 * @param lineToTranslate
+	 * @return
+	 */
 	private List<TranslateResult> generateTranslateResult(Set<String> lineToTranslate) {
 		List<TranslateResult> translateResults = new ArrayList<TranslateResult>();
 		int i =0;
@@ -121,6 +178,11 @@ public class CaveController {
 	}
 
 
+	/**
+	 * 获得需要翻译的文本
+	 * @param line
+	 * @return
+	 */
 	private Set<String> getTranslateLine(String line) {
 		Set<String> translateLine = new HashSet<String>();
 		String regex = "'(.*)'";
@@ -133,6 +195,12 @@ public class CaveController {
 		return translateLine;
 	}
 
+	/**
+	 * 正则表达式获得匹配内容
+	 * @param sourceString
+	 * @param regex
+	 * @return
+	 */
 	public List<String> getMatchString(String sourceString,String regex) {
 		List<String> matchString = new ArrayList<>();
 
@@ -145,6 +213,11 @@ public class CaveController {
 		}
 		return matchString;
 	}
+	/**
+	 * 判断字符串是否包含中文
+	 * @param str
+	 * @return
+	 */
 	public static boolean isContainChinese(String str) {
 		Pattern p = Pattern.compile("[\u4e00-\u9fa5]");
 		Matcher m = p.matcher(str);
@@ -154,14 +227,20 @@ public class CaveController {
 		return false;
 	}
 
+	/**
+	 * 固定替换处理
+	 * @param inLine
+	 * @return
+	 */
 	private String autoReplaceDeal(String inLine) {
-		inLine = inLine.replaceAll("'警告'", "dialogMsg.warn");
-		inLine = inLine.replaceAll("'确认'", "dialogMsg.confirm");
-		inLine = inLine.replaceAll("'请选择要删除的数据！'", "dialogMsg.delWarnInfo");
-		inLine = inLine.replaceAll("'您确认想要删除(.*)'", "dialogMsg.delConfirmInfo");
+		inLine = inLine.replaceAll("['|\"]提示['|\"]", "dialogMsg.info");
+		inLine = inLine.replaceAll("['|\"]警告['|\"]", "dialogMsg.warn");
+		inLine = inLine.replaceAll("['|\"]确认['|\"]", "dialogMsg.confirm");
+		inLine = inLine.replaceAll("['|\"]请选择要删除的数据！['|\"]", "dialogMsg.delWarnInfo");
+		inLine = inLine.replaceAll("['|\"]您确认想要删除(.*)['|\"]", "dialogMsg.delConfirmInfo");
 		//inLine = inLine.replaceAll("[^" + "'*\\+\\+\\+'" + "]", "dialogMsg.processingInfo");
-		inLine = inLine.replaceAll("'请选择一条要修改(.*)'", "dialogMsg.edtWarnInfo");
-		inLine = inLine.replaceAll(",'(.*)\\+{3,}'",",dialogMsg.processingInfo");
+		inLine = inLine.replaceAll("['|\"]请选择一条要修改(.*)['|\"]", "dialogMsg.edtWarnInfo");
+		inLine = inLine.replaceAll(",['|\"](.*)\\+{3,}['|\"]",",dialogMsg.processingInfo");
 		//inLine = inLine.replaceAll("'正在提交\\+\\+\\+'", "dialogMsg.processingInfo");
 		return inLine;
 	}
@@ -177,10 +256,46 @@ public class CaveController {
 	 * @param path
 	 * @throws IOException
 	 */
-	private void jsoupDeal(String path) throws IOException {
+	private Map<String,String> jsoupDeal(String path) throws IOException {
 		File input = new File(path);
 		Document doc = Jsoup.parse(input, "UTF-8");
-
+		
+		Map<String,String> chinese = new HashMap<String,String>();
+		//th标签属性
+		Elements allTh = doc.getElementsByTag("th");
+		for (Element th : allTh) {
+			String options = th.attr("data-options");
+			options = "{" + options + "}";
+			JSONObject jsonObject = JSONObject.parseObject(options);
+			if (jsonObject.getString("field").trim().length() > 0) {
+				chinese.put(jsonObject.getString("title"), jsonObject.getString("field"));
+			}
+		}
+		//input标签属性
+		Elements allInput = doc.getElementsByTag("input");
+		for (Element inputTag : allInput) {
+			String options = inputTag.attr("data-options");
+			options = "{" + options + "}";
+			JSONObject jsonObject = JSONObject.parseObject(options);
+			chinese.put(jsonObject.getString("label"), jsonObject.getString("name"));
+		}
+		//a标签包裹值
+		Elements allATag = doc.getElementsByTag("a");
+		for (Element aTag : allATag) {
+//			chinese.put(aTag.text(), aTag.attr("id"));
+			chinese.put(aTag.text(), "");
+		}
+		//legend标签包裹值
+		Elements allLegendTag = doc.getElementsByTag("a");
+		for (Element legend : allLegendTag) {
+			chinese.put(legend.text(), "");
+		}
+		
+		
+		
+		
+		return chinese;
+		/*
 		List<String> reultLines = new ArrayList<>();
 		List<String> list = FileUtils.readLines(new File(path));
 		StringBuilder sb = new StringBuilder();
@@ -193,6 +308,7 @@ public class CaveController {
 		String resultPath = path.split("\\.")[0] + "_res_" + ".jsp";
 		File resultFile = new File(resultPath);
 		FileUtils.writeLines(resultFile, list);
+		*/
 	}
 
 }
--- exoskeleton/src/main/java/com/wangxy/exoskeleton/controller/ReplaceTest.java
+++ exoskeleton/src/main/java/com/wangxy/exoskeleton/controller/ReplaceTest.java
@@ -1,10 +1,117 @@
 package com.wangxy.exoskeleton.controller;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import com.alibaba.fastjson.JSONObject;
+
 public class ReplaceTest {
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException{
         System.out.println("hello");
-        String line = "asynExecute('bfm.basedata.virtualcombi.VirtualCombiUpdate.service?resCode=virtualCombi&opCode=delService',res,succ, null,'正在删除+++') ;";
-        System.out.println(line.replaceAll(",'(.*)\\+{3,}'",",processing"));
+        String line = "asynExecute('bfm.basedata.virtualcombi.VirtualCombiUpdate.service?resCode=virtualCombi&opCode=delService',res,succ, null,'1正在删除+++') ;";
+//        System.out.println(line.replaceAll(",'(.*)\\+{3,}'",",processing"));
+        Set<String> translateLine = getTranslateLine(line);
+        for (String string : translateLine) {
+			System.out.println(string);
+		}
+        
+        String path = "G:/同步dev/国际化/virtualcombiBefore/virtualCombiList.jsp";
+        jsoupDeal(path);
+        
 
     }
+    
+    
+    /**
+	 * 获取固定情况下的中文，field，翻译作为集合
+	 * 
+	 * @param path
+	 * @throws IOException
+	 */
+	private static void jsoupDeal(String path) throws IOException {
+		File input = new File(path);
+		Document doc = Jsoup.parse(input, "UTF-8");
+		
+		Map<String,String> chinese = new HashMap<String,String>();
+		
+		Elements allTh = doc.getElementsByTag("th");
+		for (Element th : allTh) {
+			  String options = th.attr("data-options");
+			  options = "{"+options+"}";
+			  JSONObject jsonObject = JSONObject.parseObject(options);
+			  chinese.put(jsonObject.getString("title"), jsonObject.getString("field"));
+		}
+		/*
+		List<String> reultLines = new ArrayList<>();
+		List<String> list = FileUtils.readLines(new File(path));
+		StringBuilder sb = new StringBuilder();
+		for (String ss : list) {
+			sb.append(ss + "\n");
+			// 根据指定情况替换
+
+		}
+		System.out.println(sb.toString());
+		String resultPath = path.split("\\.")[0] + "_res_" + ".jsp";
+		File resultFile = new File(resultPath);
+		FileUtils.writeLines(resultFile, list);
+		*/
+	}
+	private static Set<String> getTranslateLine(String line) {
+		Set<String> translateLine = new HashSet<String>();
+		String regex = "'[^,]*'";
+//		String regex = "['](.*?)[']";
+		List<String> matchString = getMatchString(line, regex);
+		for (String matchWord : matchString) {
+			if (isContainChinese(matchWord)){
+				translateLine.add(matchWord);
+			}
+		}
+		return translateLine;
+	}
+	
+	/**
+	 * 正则表达式获得匹配内容
+	 * @param sourceString
+	 * @param regex
+	 * @return
+	 */
+	public static List<String> getMatchString(String sourceString,String regex) {
+		List<String> matchString = new ArrayList<>();
+
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(sourceString);
+		if(matcher.find()){
+			for(int i=0; i<=matcher.groupCount(); i++){
+				matchString.add(matcher.group(i));
+			}
+		}
+		return matchString;
+	}
+	/**
+	 * 判断字符串是否包含中文
+	 * @param str
+	 * @return
+	 */
+	public static boolean isContainChinese(String str) {
+		Pattern p = Pattern.compile("[\u4e00-\u9fa5]");
+		Matcher m = p.matcher(str);
+		if (m.find()) {
+			return true;
+		}
+		return false;
+	}
+
 }
